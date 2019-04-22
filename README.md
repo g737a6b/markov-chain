ca67971038254bb3940ce1121e3ea6dce78bb5bf
Hiroyuki Suzuki <github@mofg.net>
2019-04-22 16:02:04 +0900

    MeCabの利用

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # markov-chain
+
+```sh
+brew install python swig
+brew install mecab mecab-ipadic
+```
+
+```sh
+pip3 install mecab-python3 markovify
+```
+
+```sh
+vi original.txt
+python3 prepare.py
+python3 generate.py
+```
---
+++ prepare.py
@@ -0,0 +1,32 @@
+import os
+import MeCab
+
+mecab = MeCab.Tagger("-Ochasen")
+
+def split(text):
+	node = mecab.parseToNode(text)
+	keywords = []
+
+	# mecab-python3パッケージのバグ対応(2019/4/22時点で必要)
+	# See https://github.com/SamuraiT/mecab-python3/issues/19
+	current = ""
+	previous = ""
+
+	while node:
+		current = node.surface.strip("\n")
+		diff = previous[:len(previous) - len(current)]
+		if len(diff): keywords.append(diff)
+		previous = current
+		node = node.next
+	return " ".join(keywords);
+
+def main():
+	result_file_path = "./input.txt"
+	if os.path.exists(result_file_path): os.remove(result_file_path)
+	result_file = open(result_file_path, "a")
+	with open("./original.txt", "r") as file:
+		for line in file:
+			result_file.write(split(line) + "\n")
+	result_file.close()
+
+main()
